e3fccdfb502ee1e4d4567c30bd1990e9f3b14482
LAB11_USER ON WS38 <L11X09W38@citulab.net>
2024-04-19 10:27:03 +0800

    first commit

---
+++ src/main/java/com/example/login/HelloController.java
@@ -0,0 +1,54 @@
+package com.example.login;
+
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HelloController {
+
+    public Button btnLogout;
+    public VBox pnLogin;
+    private Label welcomeText;
+    public Pane pnLogout;
+    public ColorPicker cpPicker;
+    public Pane pnRegisterForm;
+
+    @FXML
+    protected void onHelloButtonClick() throws IOException {
+        AnchorPane p = (AnchorPane) pnLogin.getParent();
+        p.getScene().getStylesheets().clear();
+//        p.getScene().getStylesheets().add(getClass().getResource("userStyle.css").toExternalForm());
+        Parent scene = FXMLLoader.load(getClass().getResource("homeView.fxml"));
+        p.getChildren().clear();
+        p.getChildren().add(scene);
+    }
+
+    public void onLogout(ActionEvent actionEvent) throws IOException{
+        AnchorPane p = (AnchorPane) pnLogout.getParent();
+        Parent scene = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        p.getChildren().clear();
+        p.getChildren().add(scene);
+    }
+
+    public void onRegisterViewButtonClick() throws IOException {
+        AnchorPane p = (AnchorPane) pnLogin.getParent();
+        p.getScene().getStylesheets().clear();
+        Parent scene = FXMLLoader.load(getClass().getResource("register-view.fxml"));
+        p.getChildren().clear();
+        p.getChildren().add(scene);
+    }
+}
---
+++ src/main/java/com/example/login/RegisterView.java
@@ -0,0 +1,33 @@
+package com.example.login;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+public class RegisterView {
+    public Label registerName;
+
+    public PasswordField registerPass;
+    public String RegisterName()
+    {
+        return
+    }
+    public static void main(String[] args)
+    {
+        try(Connection c = MySQLConnection.getConnection();
+            PreparedStatement statement = c.prepareStatement(
+                    "INSERT into users (name, email) VALUES (?,?)")){
+            String name = "Alden Niño N. Cabrera";
+            String email = "alden.cabrera@cit.edu";
+            statement.setString(1,name);
+            statement.setString(2,email);
+            int rows = statement.executeUpdate();
+            System.out.println("Rows inserted: " + rows);
+        }
+        catch(SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
